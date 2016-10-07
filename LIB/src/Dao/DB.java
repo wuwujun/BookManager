db485c3296a25fa212ef147800161e69a66644ab
wuwujun <sqq0103@gmail.com>
2016-10-07 23:51:05 +0800

    sixth commit

--- LIB/src/Dao/DB.java
+++ LIB/src/Dao/DB.java
@@ -8,7 +8,7 @@ import com.mysql.jdbc.Connection;
 public class DB {
 	public Connection getConn() {
 		String Driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost:3306/bookdb";
+		String url = "jdbc:mysql://localhost:3306/bookdb?useUnicode=true&characterEncoding=UTF-8";
 		String username = "root";
 		String password = "19970103";
 		Connection conn = null;
